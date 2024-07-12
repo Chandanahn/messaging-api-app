495662a6d35dd59e1b44f4e3db0342fc97c66540
Chandana Chandana <chandanc@dhcp-10-120-174-239.wifi.oracle.com>
2024-07-12 15:03:22 +0530

    another commitf

--- src/main/java/com/training/oracle/bms/MessagingApiAppApplication.java
+++ src/main/java/com/training/oracle/bms/MessagingApiAppApplication.java
@@ -5,9 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MessagingApiAppApplication {
+	
+	public String printGreetings () {
+		return "Hi Oracle";
+	}
 
-	public static void main(String[] args) {
+	public static void  main(String[] args) {
 		SpringApplication.run(MessagingApiAppApplication.class, args);
+		System.out.println("Chandana");
+		
+		
 	}
 
 }
