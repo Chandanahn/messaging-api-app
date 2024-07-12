be40d5074708238d4c728a9720e0e8ee1669c510
Chandana Chandana <chandanc@dhcp-10-120-174-239.wifi.oracle.com>
2024-07-12 14:27:06 +0530

    commit

--- src/test/java/com/training/oracle/bms/MessagingApiAppApplicationTests.java
+++ src/test/java/com/training/oracle/bms/MessagingApiAppApplicationTests.java
@@ -2,12 +2,14 @@ package com.training.oracle.bms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class MessagingApiAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(190,200);
+		
 	}
 
 }
